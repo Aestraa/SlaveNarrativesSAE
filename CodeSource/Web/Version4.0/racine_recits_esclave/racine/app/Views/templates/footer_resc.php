e0aca5522fe39983a91ef0b11338387000ea2cfc
james <jamus18.JG@gmail.com>
2023-10-09 11:52:17 +0200

    traduction footer

--- CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/templates/footer_resc.php
+++ CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/templates/footer_resc.php
@@ -1,8 +1,8 @@
 <br><br><br><br>
 <footer>
   <div class="text-center p-3">
-    <a id="lienfoot" href="<?= site_url()."about" ?>">À propos</a>
-    <a id="lienfoot" href="<?= site_url()."contact" ?>">Contact</a> 
+    <a id="lienfoot" href="<?= site_url()."about" ?>"><?= lang('footer_resc.about') ?></a>
+    <a id="lienfoot" href="<?= site_url()."contact" ?>"><?= lang('footer_resc.contact') ?></a> 
   </div>
 </footer> 
 
