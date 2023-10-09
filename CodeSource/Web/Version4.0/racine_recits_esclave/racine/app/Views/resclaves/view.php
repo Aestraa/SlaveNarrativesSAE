4920aadb002e00ccbf09395c5382296973d98501
james <jamus18.JG@gmail.com>
2023-10-09 23:45:10 +0200

    traduction view fiche récit

--- CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/view.php
+++ CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/view.php
@@ -66,7 +66,7 @@ curl_close($curl);
 <br><br>
 
 <p style="text-align:center; font-size:25px;padding:6px;">  
-    Fiche récit</p>
+    <?= lang('view.title') ?></p>
 
     <div class="rec"><br>
     <p style="text-align:center; font-size:25px; font-style:italic;padding:6px;">
@@ -77,40 +77,40 @@ curl_close($curl);
     <div class="rec"><br>
     
         <div class="rec_par">
-        <strong><p style="text-align:right;">Année de publication :</strong> <?= esc($rec['date_publi']) ?> </p>
-        <strong><p style="text-align:right;">Mode de publication :</strong> <?= esc($rec['mode_publi']) ?> </p>
-        <strong><p style="text-align:right;">Plusieurs recits écrits :</strong> <?= esc($rec['plrs_recits']) ?> </p>
+        <strong><p style="text-align:right;"><?= lang('view.year_publication') ?> :</strong> <?= esc($rec['date_publi']) ?> </p>
+        <strong><p style="text-align:right;"><?= lang('view.method_publication') ?> :</strong> <?= esc($rec['mode_publi']) ?> </p>
+        <strong><p style="text-align:right;"><?= lang('view.several_written_narratives') ?> :</strong> <?= esc($rec['plrs_recits']) ?> </p>
 
-    <strong><p>Nom de l'esclave :</strong> <?= esc($rec['nom_esc']) ?> </p>
-    <strong><p>Type de récit :</strong> <?= esc($rec['type_recit']) ?> </p>
+    <strong><p><?= lang('view.name_slave') ?> :</strong> <?= esc($rec['nom_esc']) ?> </p>
+    <strong><p><?= lang('view.type_narrative') ?> :</strong> <?= esc($rec['type_recit']) ?> </p>
 
-    <strong><p>Date de naissance :</strong> <?= esc($rec['naissance']) ?> </p>
-    <strong><p>Lieu de publication :</strong> <?= esc($rec['lieu_publi']) ?> </p>
-    <strong><p>Origine des parents :</strong> <?= esc($rec['origine_parents']) ?> </p>
-    <strong><p>Nom du scribe/écrivain :</strong> <?= esc($rec['scribe_editeur']) ?> </p>
-    <strong><p>Information supplémentaire :</strong> <?= esc($rec['particularites']) ?> </p>
+    <strong><p><?= lang('view.date_birth') ?> :</strong> <?= esc($rec['naissance']) ?> </p>
+    <strong><p><?= lang('view.location_publication') ?> :</strong> <?= esc($rec['lieu_publi']) ?> </p>
+    <strong><p><?= lang('view.origins_parents') ?> :</strong> <?= esc($rec['origine_parents']) ?> </p>
+    <strong><p><?= lang('view.name_writer') ?> :</strong> <?= esc($rec['scribe_editeur']) ?> </p>
+    <strong><p><?= lang('view.additional_information') ?> :</strong> <?= esc($rec['particularites']) ?> </p>
 
 </div>
 <br><br>
 
 <div id="comm">
     <p style="text-align:center;">
-        Commentaires / Historiographie: <br><br> 
+    <?= lang('view.comments') ?> : <br><br> 
         <?= html_entity_decode($rec['historiographie']) ?>
 
 </p>
 </div>
 
 <br>
-<p>Lien vers le récit : <a href="<?= esc($rec['lien_recit']) ?>"><?= esc($rec['lien_recit']) ?></a></p>
+<p><?= lang('view.link_narrative') ?> : <a href="<?= esc($rec['lien_recit']) ?>"><?= esc($rec['lien_recit']) ?></a></p>
 
 
 <br><br>
 <div style="display:flex;">
 <button class="button_return" onclick='window.location.href = 
-"<?= site_url()."recits" ?>"'><p>RETOUR VERS LA LISTE DES RÉCITS</p></button>
+"<?= site_url()."recits" ?>"'><p><?= lang('view.back_narratives_list_button') ?></p></button>
 <button class="button_return" onclick='window.location.href = 
-"<?= site_url()."map" ?>"'><p>RETOUR VERS LA CARTE DES RECITS</p></button>
+"<?= site_url()."map" ?>"'><p><?= lang('view.back_narratives_map_button') ?></p></button>
 <br><br>
 </div>
 <br>
