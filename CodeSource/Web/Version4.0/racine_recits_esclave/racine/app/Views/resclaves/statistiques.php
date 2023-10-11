aff4259401b78daccb347e034f1ada532dc527fb
james <jamus18.JG@gmail.com>
2023-10-11 10:59:20 +0200

    ajout box pour stats

--- CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/statistiques.php
+++ CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/statistiques.php
@@ -6,10 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php $page_name = 'Statistiques'?>
     <title><?= $page_name ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style_connexion.css'); ?>">
-</head>
+ </head>
 
 <body>
+
+<div class="stats-container">
+    <div class="box-stats">Box 1</div>
+    <div class="box-stats">Box 2</div>
+    <div class="box-stats">Box 3</div>
+  </div>
   
 </body>
 
