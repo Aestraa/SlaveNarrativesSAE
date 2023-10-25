ae06519f8f50b63ac9831c15da787461cb980c88
Aestraa <moinyh42@gmail.com>
2023-10-25 16:05:05 +0200

    AjoutLien: AjoutCSS + Bouton Retour + Ajustements

--- CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/ajout_link.php
+++ CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/ajout_link.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= lang('ajout_link.title') ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style_ajout_link.css'); ?>">
 </head>
+
 <body>
     <div id="page">
         <div id='block1'>
@@ -15,6 +17,7 @@
                 <input type="text" id="champ1" name="champ1"><br>
                 <label for="champ2"><?= lang('ajout_link.tab1_ch2') ?></label>
                 <input type="text" id="champ2" name="champ2"><br><br>
+                <a class="retour" href="<?= site_url('/recits') ?>"><?= lang('recits.bouton_retour') ?></a></p>
                 <button type="button" id="addLinkButton"><?= lang('ajout_link.tab1_add') ?></button>
             </form>
         </div>
@@ -30,7 +33,7 @@
                         </tr>
                     </thead>
                     <tbody id="linkTable">
-                            <!-- Les coordonnées seront ajoutées ici -->
+                        <!-- Les coordonnées seront ajoutées ici -->
                     </tbody>
                 </table>
                 <input type="hidden" name="ref" id="refinput">
@@ -42,62 +45,63 @@
     </div>
 
     <script>
-        document.addEventListener("DOMContentLoaded", function () {
-            var refs = [];
-            var links = [];
+    document.addEventListener("DOMContentLoaded", function() {
+        var refs = [];
+        var links = [];
 
-            var addLinkButton = document.getElementById("addLinkButton");
-            var linkTable = document.getElementById("linkTable");
-            var refInput = document.getElementById("refinput");
-            var linkInput = document.getElementById("linkinput");
+        var addLinkButton = document.getElementById("addLinkButton");
+        var linkTable = document.getElementById("linkTable");
+        var refInput = document.getElementById("refinput");
+        var linkInput = document.getElementById("linkinput");
 
-            addLinkButton.addEventListener("click", function () {
-                var champ1Value = document.getElementById("champ1").value;
-                var champ2Value = document.getElementById("champ2").value;
+        addLinkButton.addEventListener("click", function() {
+            var champ1Value = document.getElementById("champ1").value;
+            var champ2Value = document.getElementById("champ2").value;
 
-                // Créez un tableau pour stocker les données
-                var data = [champ1Value, champ2Value];
-                // Ajoutez les données au tableau
-                refs.push(data[0]);
-                links.push(data[1]);
+            // Créez un tableau pour stocker les données
+            var data = [champ1Value, champ2Value];
+            // Ajoutez les données au tableau
+            refs.push(data[0]);
+            links.push(data[1]);
 
-                var newRow = linkTable.insertRow();
-                var cell1 = newRow.insertCell(0);
-                var cell2 = newRow.insertCell(1);
-                var deleteCell = newRow.insertCell(2);
+            var newRow = linkTable.insertRow();
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var deleteCell = newRow.insertCell(2);
 
-                cell1.innerHTML = data[0];
-                cell2.innerHTML = data[1];
+            cell1.innerHTML = data[0];
+            cell2.innerHTML = data[1];
 
-                // Ajouter un bouton de suppression pour la nouvelle ligne
-                var deleteButton = document.createElement("button");
-                deleteButton.textContent = "Supprimer";
-                deleteButton.addEventListener("click", function () {
-                    if (linkTable.rows.length > 1) {
-                        var index = Array.from(linkTable.rows).indexOf(newRow);
-                        // Supprimez la ligne du tableau
-                        linkTable.deleteRow(newRow.rowIndex);
-                        // Supprimez les données correspondantes du tableau
-                        refs.splice(index, 1);
-                        links.splice(index, 1);
-                        updateHiddenInputs();
-                    }
-                });
-                deleteCell.appendChild(deleteButton);
-
-                // Effacer les valeurs des champs du formulaire 1
-                document.getElementById("champ1").value = "";
-                document.getElementById("champ2").value = "";
-
-                updateHiddenInputs();
+            // Ajouter un bouton de suppression pour la nouvelle ligne
+            var deleteButton = document.createElement("button");
+            deleteButton.textContent = "Supprimer";
+            deleteButton.addEventListener("click", function() {
+                if (linkTable.rows.length > 1) {
+                    var index = Array.from(linkTable.rows).indexOf(newRow);
+                    // Supprimez la ligne du tableau
+                    linkTable.deleteRow(newRow.rowIndex);
+                    // Supprimez les données correspondantes du tableau
+                    refs.splice(index, 1);
+                    links.splice(index, 1);
+                    updateHiddenInputs();
+                }
             });
+            deleteCell.appendChild(deleteButton);
 
-            // Fonction pour mettre à jour les champs cachés avec les données du tableau
-            function updateHiddenInputs() {
-                refInput.value = JSON.stringify(refs);
-                linkInput.value = JSON.stringify(links);
-            }
+            // Effacer les valeurs des champs du formulaire 1
+            document.getElementById("champ1").value = "";
+            document.getElementById("champ2").value = "";
+
+            updateHiddenInputs();
         });
+
+        // Fonction pour mettre à jour les champs cachés avec les données du tableau
+        function updateHiddenInputs() {
+            refInput.value = JSON.stringify(refs);
+            linkInput.value = JSON.stringify(links);
+        }
+    });
     </script>
 </body>
+
 </html>
--- CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/modif_recit.php
+++ CodeSource/Web/Version4.0/racine_recits_esclave/racine/app/Views/resclaves/modif_recit.php
@@ -1,144 +1,146 @@
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?= lang('modif_recit.title') ?></title> 
+    <title><?= lang('modif_recit.title') ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style_connexion.css'); ?>">
 </head>
+
 <body>
-<a class="retour" href="<?= site_url('/recits') ?>"><?= lang('recits.bouton_retour') ?></a></p>
+
     <div class="login-container">
-           <form action="<?= site_url('Modif/ModifPoly_Recit?idR='.$_GET['idR']) ?>" method="post">
-           <label><?= lang('modif_recit.name_narrative') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="nomR" id="nomR" type="text" value="'. $elt['titre'] .'" required/><br><br>';
-                            }
+        <form action="<?= site_url('Modif/ModifPoly_Recit?idR=' . $_GET['idR']) ?>" method="post">
+            <label><?= lang('modif_recit.name_narrative') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="nomR" id="nomR" type="text" value="' . $elt['titre'] . '" required/><br><br>';
+                    }
+                }
+            }
+            ?>
+            <label><?= lang('modif_recit.name_slave') ?></label>
+            <select name="idE" id="idE" required>
+                <?php
+                if (!empty($auteurs) && is_array($auteurs)) {
+                    foreach ($auteurs as $elt) {
+                        if ($elt['id_auteur'] == $_GET['esc']) {
+                            echo '<option value="' . $elt['id_auteur'] . '"selected>' . $elt['nom'] . ' </option>';
+                        } else {
+                            echo '<option value="' . $elt['id_auteur'] . '">' . $elt['nom'] . ' </option>';
                         }
                     }
-            ?>
-           <label><?= lang('modif_recit.name_slave') ?></label>
-           <select name="idE" id="idE" required>
-                    <?php
-                    if (!empty($auteurs) && is_array($auteurs)) {
-                        foreach ($auteurs as $elt) {
-                            if ($elt['id_auteur'] == $_GET['esc']){
-                                echo '<option value="' . $elt['id_auteur'] . '"selected>' . $elt['nom'] . ' </option>'; 
-                            } else {
-                                echo '<option value="' . $elt['id_auteur'] . '">' . $elt['nom'] . ' </option>';
-                            }
-                        }  
-                    }
-                    ?>
-                </select><br><br>
+                }
+                ?>
+            </select><br><br>
 
-           <label><?= lang('modif_recit.location_publication') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="lieuP" id="lieuP" type="text" value="'. $elt['lieu_publi'] .'" required/><br><br>';
-                            }
-                        }
+            <label><?= lang('modif_recit.location_publication') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="lieuP" id="lieuP" type="text" value="' . $elt['lieu_publi'] . '" required/><br><br>';
                     }
-            ?>
-           
-
-           <label><?= lang('modif_recit.year_publication') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="dateP" id="dateP" type="number" min="0" max="2100" value="'. $elt['date_publi'] .'" required/><br><br>';
-                            }
-                        }
-                    }
-            ?>
-        
-           <label><?= lang('modif_recit.type_narrative') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="typeR" id="typeR" type="text" value="'. $elt['lieu_publi'] .'" required/><br><br>';
-                            }
-                        }
-                    }
-            ?>
-        
-           <label><?= lang('modif_recit.comments') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                                echo "<input name='com' id='com' type='text' value='" . htmlspecialchars($elt['historiographie']) . "' required/><br><br>";
-
-                            }
-                        }
-                    }
+                }
+            }
             ?>
 
-           <label><?= lang('modif_recit.method_publication') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="modeP" id="modeP" type="text" value="'. $elt['mode_publi'] .'" required/><br><br>';
-                            }
-                        }
+
+            <label><?= lang('modif_recit.year_publication') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="dateP" id="dateP" type="number" min="0" max="2100" value="' . $elt['date_publi'] . '" required/><br><br>';
                     }
+                }
+            }
             ?>
-        
-          <!--<label>Date de naissance :</label>
+
+            <label><?= lang('modif_recit.type_narrative') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="typeR" id="typeR" type="text" value="' . $elt['lieu_publi'] . '" required/><br><br>';
+                    }
+                }
+            }
+            ?>
+
+            <label><?= lang('modif_recit.comments') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo "<input name='com' id='com' type='text' value='" . htmlspecialchars($elt['historiographie']) . "' required/><br><br>";
+                    }
+                }
+            }
+            ?>
+
+            <label><?= lang('modif_recit.method_publication') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="modeP" id="modeP" type="text" value="' . $elt['mode_publi'] . '" required/><br><br>';
+                    }
+                }
+            }
+            ?>
+
+            <!--<label>Date de naissance :</label>
            <input name="dateN" id="dateN" type="date" /><br><br> -->
 
-           <label><?= lang('modif_recit.name_writer') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="nomS" id="nomS" type="text" value="'. $elt['scribe_editeur'] .'" required/><br><br>';
-                            }
-                        }
+            <label><?= lang('modif_recit.name_writer') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="nomS" id="nomS" type="text" value="' . $elt['scribe_editeur'] . '" required/><br><br>';
                     }
+                }
+            }
             ?>
-        
-           <label><?= lang('modif_recit.link_narrative') ?></label>
-           <?php
-                    if (!empty($title) && is_array($title)) {
-                        foreach ($title as $elt) {
-                            if ($elt['id_recit'] == $_GET['idR']){
-                            echo '<input name="lienR" id="lienR" type="text" value="'. $elt['lien_recit'] .'" required/><br><br>';
-                            }
-                        }
+
+            <label><?= lang('modif_recit.link_narrative') ?></label>
+            <?php
+            if (!empty($title) && is_array($title)) {
+                foreach ($title as $elt) {
+                    if ($elt['id_recit'] == $_GET['idR']) {
+                        echo '<input name="lienR" id="lienR" type="text" value="' . $elt['lien_recit'] . '" required/><br><br>';
                     }
+                }
+            }
             ?>
 
             <label><?= lang('ajout_recit.choix_polys') ?></label>
             <select name="poly[]" id="poly" multiple required>
-                    <?php
-                    if (!empty($polys) && is_array($polys)) {
-                        foreach ($polys as $elt) {
-                            $write = true;
-                            if (!empty($recitP) && is_array($recitP)) {
-                                foreach($recitP as $pol){
-                                    if($pol['poly_id'] == $elt['id'] && $pol['recit_id'] == $_GET['idR']){
-                                        echo '<option value="' . $elt['id'] . '" selected>' . $elt['name']. ' </option>';
-                                        $write = false;
-                                    }
-                                }
-                                if($write){
-                                    echo '<option value="' . $elt['id'] . '">' . $elt['name']. ' </option>';
+                <?php
+                if (!empty($polys) && is_array($polys)) {
+                    foreach ($polys as $elt) {
+                        $write = true;
+                        if (!empty($recitP) && is_array($recitP)) {
+                            foreach ($recitP as $pol) {
+                                if ($pol['poly_id'] == $elt['id'] && $pol['recit_id'] == $_GET['idR']) {
+                                    echo '<option value="' . $elt['id'] . '" selected>' . $elt['name'] . ' </option>';
+                                    $write = false;
                                 }
                             }
-                        }  
+                            if ($write) {
+                                echo '<option value="' . $elt['id'] . '">' . $elt['name'] . ' </option>';
+                            }
+                        }
                     }
-                    ?>
-                </select><br><br>
-        
-           <button type="submit"><?= lang('modif_recit.modify_button') ?></button>
+                }
+                ?>
+            </select><br><br>
+
+            <a class="retour" href="<?= site_url('/recits') ?>"><?= lang('recits.bouton_retour') ?></a></p>
+            <button type="submit"><?= lang('modif_recit.modify_button') ?></button>
         </form>
         <script>
         var select = document.getElementById('poly');
@@ -163,8 +165,8 @@
                 clonedSelect.scrollTop = scrollPosition;
             }
         };
-
-    </script>
+        </script>
 
 </body>
+
 </html>
